bb4e44d02f668b79c3f28f6cb878a43c71a0948e
Aaron Lam <aaronlam2k@gmail.com>
2022-09-05 12:18:35 -0700

    Commit for Netlify froms setup

---
+++ assets/js/animation.js
@@ -0,0 +1,67 @@
+// Slide left-to-right
+function revealFromLeft() {
+    var reveals = document.querySelectorAll(".reveal-from-left");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealFromLeft);
+
+// Slide right-to-left
+function revealFromRight() {
+    var reveals = document.querySelectorAll(".reveal-from-right");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealFromRight);
+
+// Slide top-to-bottom
+function revealFromTop() {
+    var reveals = document.querySelectorAll(".reveal-from-top");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealFromTop);
+
+// Slide bottom-to-top
+function revealFromBottom() {
+    var reveals = document.querySelectorAll(".reveal-from-bottom");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealFromBottom);
